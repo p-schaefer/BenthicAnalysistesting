96b769ad1a8b09212c2aab88942bf1711d944a31
p-schaefer <patrick.schaefer@mail.utoronto.ca>
2017-03-20 16:48:31 -0400

    update

--- Testing Scripts/Combined tsa function.R
+++
@@ -1,5 +0,0 @@
-tsa.combined<-function(benthic.metric,add.mets=F, env.data, distance=NULL,
-                       outlier.rem=T, m.select=T,rank=F,na.cutoff=0.7,
-                       k=NULL, adaptive=T, RDA.reference=NULL) {
-  
-}
