29df0f4ab62eb087e5a23f6c801f13bd9388787f
Patrick Schaefer <patrick.schaefer@mail.utoronto.ca>
2023-11-29 19:54:53 -0500

    documented datasets

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Hilsenhoff Biotic Index
+"HBI_RAW"
+
+#' Canadian Ecological Flow Index
+"CEFI"
+
+#' Lookup Table of feeding traits
+"trait.feeding"
+
+#' Lookup Table of habitat traits
+"trait.habit"
